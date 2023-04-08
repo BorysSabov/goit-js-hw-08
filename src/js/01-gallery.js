79a4149ceeb9595059545dcf5d93f6661867e4f8
Borys <3040sabov@gmail.com>
2023-04-08 23:55:38 +0300

    fix-settings

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
 console.log(galleryItems);
+
+const addGalleryPic = document.querySelector('.gallery');
+
+const createGalleryEl = galleryItems
+  .map(
+    item =>
+      ` <li class="gallery__item">
+   <a class="gallery__link" href="${item.original}">
+      <img
+      class="gallery__image"
+      src="${item.preview}"
+      alt="${item.description}"
+      />
+   </a>
+</li>`
+  )
+  .join('');
+
+addGalleryPic.innerHTML = createGalleryEl;
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionDelay: 250,
+  captionsData: 'alt',
+});
